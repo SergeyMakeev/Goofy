6922358bed04fd5cc3b85f4b83d3df5635080fd6
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2024-01-06 13:08:29 +0100

    print current path

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,11 +1,8 @@
 #include <filesystem>
 #include <iostream>
-#include <string>
 namespace fs = std::filesystem;
 
 int main()
 {
-    std::string path = ".";
-    for (const auto& entry : fs::directory_iterator(path))
-        std::cout << entry.path() << std::endl;
+    std::cout << "Current path is " << fs::current_path() << '\n';
 }
